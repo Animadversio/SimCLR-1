3dc72e1bca35de1ef0c21879887b4dd01868d25a
binxu <binxu.wang@wustl.edu>
2021-10-04 03:13:25 -0400

    add

--- data_aug/dataset_w_salmap.py
+++ data_aug/dataset_w_salmap.py
@@ -1,14 +1,17 @@
 
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
 from torchvision import datasets, transforms, utils
 import numpy as np
 import pandas as pd
 from os.path import join
 import matplotlib.pylab as plt
+from data_aug.gaussian_blur import GaussianBlur
+from data_aug.view_generator import ContrastiveLearningViewGenerator
 
 class STL10_w_salmap(Dataset):
-    """Face Landmarks dataset."""
+    """ Return STL image with saliency maps """
 
     def __init__(self, dataset_dir=r"/scratch1/fs1/crponce/Datasets", transform=None, split="unlabeled"):
         """
@@ -38,6 +41,62 @@ class STL10_w_salmap(Dataset):
             img = self.transform(img)
         salmap_tsr = F.interpolate(torch.tensor(salmap).unsqueeze(0),[224,224]).float()
         return (img, salmap), label  # labels can be dropped.
+
+
+from saliency_random_cropper import RandomResizedCrop_with_Density, RandomCrop_with_Density
+class Contrastive_STL10_w_salmap(Dataset):
+    """ Return Crops of STL10 images with saliency maps """
+
+    def __init__(self, dataset_dir=r"/scratch1/fs1/crponce/Datasets", \
+        density_cropper=RandomResizedCrop_with_Density((96, 96),), \
+        transform_post_crop=None, split="unlabeled", n_views=2):
+        """
+        Args:
+            dataset_dir (string): Directory with all the images. E:\Datasets
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+            split: "unlabeled"
+        """
+        self.dataset = datasets.STL10(dataset_dir, split=split, download=True,
+                                 transform=None,)
+        self.salmaps = np.load(join(dataset_dir, "stl10_unlabeled_salmaps_salicon.npy")) # stl10_unlabeled_saliency.npy
+        assert len(self.dataset) == self.salmaps.shape[0]
+        self.root_dir = dataset_dir
+        self.density_cropper = density_cropper
+        if transform_post_crop is not None:
+            self.transform = transform_post_crop
+        else:
+            self.transform = get_simclr_post_crop_transform(96, s=1, blur=True)
+        self.n_views = n_views
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, label = self.dataset.__getitem__(idx) # img is PIL.Image, label is xxxx 
+        salmap = self.salmaps[idx, :, :, :].astype('float') # numpy.ndarray
+        salmap_tsr = F.interpolate(torch.tensor(salmap).unsqueeze(0), [96, 96]).float()
+        sal_crops = [self.density_cropper(img, salmap_tsr) for i in range(n_views)]
+
+        if self.transform:
+            imgs = [self.transform(cropview) for cropview in sal_crops]
+            return imgs
+        else:
+            return sal_crops
+
+    @staticmethod
+    def get_simclr_post_crop_transform(size, s=1, blur=True):
+        """Return a set of data augmentation transformations as described in the SimCLR paper."""
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        data_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                              transforms.RandomApply([color_jitter], p=0.8),
+                                              transforms.RandomGrayscale(p=0.2),] +
+                                            ([GaussianBlur(kernel_size=int(0.1 * size)),]  if  blur  else  []) +
+                                              [transforms.ToTensor()])
+        # transforms.Compose([transforms.ToTensor(),
+        #                     transforms.Normalize(mean=(0.4914, 0.4822, 0.4465),
+        #                                          std=(0.2023, 0.1994, 0.2010))])
+        return data_transforms
 
 
 def visualize_samples(saldataset):
