26cea8c87867ea03bd5e579c69e763b852204a42
binxu <binxu.wang@wustl.edu>
2021-10-04 02:33:54 -0400

    implement new saliency cropper

--- data_aug/saliency_random_cropper.py
+++ data_aug/saliency_random_cropper.py
@@ -1,9 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from typing import Tuple, List, Optional
 from torchvision.transforms import functional as TF
+import math 
+import numpy as np
+from torch import Tensor
+from collections.abc import Sequence
+from typing import Tuple, List, Optional
+
 def _setup_size(size, error_msg):
   if isinstance(size, numbers.Number):
     return int(size), int(size)
@@ -111,8 +115,8 @@ class RandomCrop_with_Density(torch.nn.Module):
         For example, padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
         will result in [2, 1, 1, 2, 3, 4, 4, 3]
   """
-  def __init__(self, size, padding=None, pad_if_needed=False, fill=0, padding_mode="constant", temperature=4, 
-               device="cuda", avgpool=True, density_sigma=31,):
+  def __init__(self, size, padding=None, pad_if_needed=False, fill=0, padding_mode="constant", 
+               device="cuda", avgpool=True, temperature=4, density_sigma=31,):
     super().__init__()
 
     self.size = tuple(_setup_size(
@@ -200,3 +204,174 @@ class RandomCrop_with_Density(torch.nn.Module):
 
   def __repr__(self):
     return self.__class__.__name__ + "(size={0}, padding={1})".format(self.size, self.padding)
+
+
+
+def _overlap_area(i, j, h, w, height, width):
+  new_i = max(0, i)
+  new_j = max(0, j)
+  new_i_max = min(i + h, height)
+  new_j_max = min(j + w, width)
+  area = (new_i_max - new_i) * (new_j_max - new_j)
+  return area
+
+
+class RandomResizedCrop_with_Density(torch.nn.Module):
+  """Crop a random portion of image and resize it to a given size.
+
+  If the image is torch Tensor, it is expected
+  to have [..., H, W] shape, where ... means an arbitrary number of leading dimensions
+
+  A crop of the original image is made: the crop has a random area (H * W)
+  and a random aspect ratio. This crop is finally resized to the given
+  size. This is popularly used to train the Inception networks.
+
+  Args:
+    size (int or sequence): expected output size of the crop, for each edge. If size is an
+      int instead of sequence like (h, w), a square output size ``(size, size)`` is
+      made. If provided a sequence of length 1, it will be interpreted as (size[0], size[0]).
+
+      .. note::
+        In torchscript mode size as single int is not supported, use a sequence of length 1: ``[size, ]``.
+    scale (tuple of float): Specifies the lower and upper bounds for the random area of the crop,
+      before resizing. The scale is defined with respect to the area of the original image.
+    ratio (tuple of float): lower and upper bounds for the random aspect ratio of the crop, before
+      resizing.
+    interpolation (InterpolationMode): Desired interpolation enum defined by
+      :class:`torchvision.transforms.InterpolationMode`. Default is ``InterpolationMode.BILINEAR``.
+      If input is Tensor, only ``InterpolationMode.NEAREST``, ``InterpolationMode.BILINEAR`` and
+      ``InterpolationMode.BICUBIC`` are supported.
+      For backward compatibility integer values (e.g. ``PIL.Image.NEAREST``) are still acceptable.
+
+  """
+
+  def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=InterpolationMode.BILINEAR,
+      temperature=1.5, pad_if_needed=False):
+    super().__init__()
+    self.size = _setup_size(size, error_msg="Please provide only two dimensions (h, w) for size.")
+
+    if not isinstance(scale, Sequence):
+      raise TypeError("Scale should be a sequence")
+    if not isinstance(ratio, Sequence):
+      raise TypeError("Ratio should be a sequence")
+    if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+      warnings.warn("Scale and ratio should be of kind (min, max)")
+
+    # Backward compatibility with integer value
+    if isinstance(interpolation, int):
+      warnings.warn(
+        "Argument interpolation should be of type InterpolationMode instead of int. "
+        "Please, use InterpolationMode enum."
+      )
+      interpolation = _interpolation_modes_from_int(interpolation)
+
+    self.interpolation = interpolation
+    self.scale = scale
+    self.ratio = ratio
+    self.temperature = temperature
+    self.pad_if_needed = pad_if_needed
+
+
+  @staticmethod
+  def get_params(
+      img: Tensor, scale: List[float], ratio: List[float], density=None
+  ) -> Tuple[int, int, int, int]:
+    """Get parameters for ``crop`` for a random sized crop.
+
+    Args:
+      img (PIL Image or Tensor): Input image.
+      scale (list): range of scale of the origin size cropped
+      ratio (list): range of aspect ratio of the origin aspect ratio cropped
+
+    Returns:
+      tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+      sized crop.
+    """
+    width, height = TF._get_image_size(img)
+    area = height * width
+
+    log_ratio = torch.log(torch.tensor(ratio))
+    for _ in range(30):
+      target_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
+      aspect_ratio = torch.exp(
+        torch.empty(1).uniform_(log_ratio[0], log_ratio[1])
+      ).item()
+
+      w = int(round(math.sqrt(target_area * aspect_ratio)))
+      h = int(round(math.sqrt(target_area / aspect_ratio)))
+      if density is not None:
+        flat_idx = torch.multinomial(density.flatten(), 1, replacement=True).cpu()
+        cnt_coord = unravel_indices(flat_idx, density[0, 0, :, :].shape)
+        ci, cj = cnt_coord[0,0], cnt_coord[0,1]
+        i, j = ci - h // 2, cj - w // 2
+        over_area = _overlap_area(i, j, h, w, height, width)
+        if (area * scale[1]) > over_area > \
+            (area * scale[0]):
+          return i, j, h, w
+
+      else: #uniform distribution. 
+        if 0 < w <= width and 0 < h <= height:
+          # the only sampling part
+          i = torch.randint(0, height - h + 1, size=(1,)).item()
+          j = torch.randint(0, width - w + 1, size=(1,)).item()
+          return i, j, h, w
+
+    # Fallback to central crop
+    in_ratio = float(width) / float(height)
+    if in_ratio < min(ratio):
+      w = width
+      h = int(round(w / min(ratio)))
+    elif in_ratio > max(ratio):
+      h = height
+      w = int(round(h * max(ratio)))
+    else:  # whole image
+      w = width
+      h = height
+    i = (height - h) // 2
+    j = (width - w) // 2
+    return i, j, h, w
+
+
+  def forward(self, img, logdensity=None):
+    """
+    Args:
+      img (PIL Image or Tensor): Image to be cropped and resized.
+
+    Returns:
+      PIL Image or Tensor: Randomly cropped and resized image.
+    """
+    if logdensity is not None:
+      densitymap = torch.exp((logdensity - torch.logsumexp(logdensity, (1,2,3), keepdim=True)) / self.temperature) # 0,
+    else: 
+      densitymap = None
+    i, j, h, w = self.get_params(img, self.scale, self.ratio, densitymap)
+    width, height = TF._get_image_size(img)
+    if self.pad_if_needed:
+      new_i, new_j = i, j
+      p_left, p_top, p_right, p_bottom = 0, 0, 0, 0
+      if i < 0: 
+        p_top = abs(i)
+        new_i = 0
+      elif i + h > height:
+        p_bottom = i + h - height
+      if j < 0:
+        p_left = abs(j)
+        new_j = 0
+      elif j + w > width:
+        p_right = j + w - width
+
+      img_pad = TF.pad(img, [p_left, p_top, p_right, p_bottom], fill=0, padding_mode="constant")
+      return TF.resized_crop(img_pad, new_i, new_j, h, w, self.size, self.interpolation)
+    else: # no padding cut off at the border. 
+      i, j = max(0, i), max(0, j)
+      h, w = min(h, height - i), min(w, width - j)
+      return TF.resized_crop(img, i, j, h, w, self.size, self.interpolation)
+
+
+  def __repr__(self):
+    interpolate_str = self.interpolation.value
+    format_string = self.__class__.__name__ + '(size={0}'.format(self.size)
+    format_string += ', scale={0}'.format(tuple(round(s, 4) for s in self.scale))
+    format_string += ', ratio={0}'.format(tuple(round(r, 4) for r in self.ratio))
+    format_string += ', interpolation={0})'.format(interpolate_str)
+    return format_string
