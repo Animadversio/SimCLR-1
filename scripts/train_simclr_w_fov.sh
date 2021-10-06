02bb37d7cdde6b667be98118df771d5fa5951b87
Animadversio <binxu.wang@wustl.edu>
2021-10-05 21:06:51 -0400

    add

--- scripts/train_simclr_w_fov.sh
+++ scripts/train_simclr_w_fov.sh
@@ -9,7 +9,7 @@
 #BSUB -R 'rusage[mem=64GB]'
 #BSUB -M 64GB
 #BSUB -u binxu.wang@wustl.edu
-#BSUB -o  /scratch1/fs1/crponce/simclr_fast_salienc_new_train.%J.%I
+#BSUB -o  /scratch1/fs1/crponce/simclr_fast_fov_exps.%J.%I
 #BSUB -a 'docker(pytorchlightning/pytorch_lightning:base-cuda-py3.9-torch1.9)'
 
 # export LSF_DOCKER_SHM_SIZE=16g
