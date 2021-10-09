d672cdb0f3c6bb07796ab2fd3182d7026b1a2268
Animadversio <binxu.wang@wustl.edu>
2021-10-09 02:51:14 -0400

    add jobs

--- scripts/train_simclr_w_magnif.sh
+++ scripts/train_simclr_w_magnif.sh
@@ -2,7 +2,7 @@
 #BSUB -n 4
 #BSUB -q general
 #BSUB -G compute-crponce
-#BSUB -J 'simclr_fast_magnif_exps[18-23]'
+#BSUB -J 'simclr_fast_magnif_exps[18-23,26-29]'
 #BSUB -gpu "num=1:gmodel=TeslaV100_SXM2_32GB:mode=exclusive_process"
 #BSUB -R 'gpuhost'
 #BSUB -R 'select[mem>48G]'
@@ -44,6 +44,10 @@ param_list=\
 --run_label proj256_eval_magnif_exp_cvr_0_05-1_50_slp_0_75-3_00 --magnif --gridfunc_form radial_exp --cover_ratio 0.05  1.50  --slope_C 0.75 3.0 --sampling_bdr 16
 --run_label proj256_eval_magnif_bsl_RND --randomize_seed --crop
 --run_label proj256_eval_magnif_cvr_0_05-0_35_RND --randomize_seed --magnif --cover_ratio 0.05 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_0_75      --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 0.75  --sampling_bdr 16
+--run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_3_00      --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 3.00  --sampling_bdr 16
+--run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_0_75-1_50 --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 0.75  1.5 --sampling_bdr 16
+--run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_0_25-1_00 --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 0.25  1.0 --sampling_bdr 16
 '
 
 export extra_param="$(echo "$param_list" | head -n $LSB_JOBINDEX | tail -1)"
