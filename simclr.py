3233e204088d60662c66e5b38009d9db25fd5980
binxu <binxu.wang@wustl.edu>
2021-10-09 19:23:12 -0400

    Add notes to core Simclr code

--- simclr.py
+++ simclr.py
@@ -37,28 +37,30 @@ class SimCLR(object):
         labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
         labels = labels.to(self.args.device)
-
+        # labels is a matrix of shape (batch * n_views, batch * n_views); 
+        # It's (batch, batch) identity matrix blocks arranged in (n_views, n_views) fashion. 
         features = F.normalize(features, dim=1)
 
-        similarity_matrix = torch.matmul(features, features.T)
+        similarity_matrix = torch.matmul(features, features.T) # cosine similarity
         # assert similarity_matrix.shape == (
         #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
         # assert similarity_matrix.shape == labels.shape
 
         # discard the main diagonal from both: labels and similarities matrix
         mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.args.device)
-        labels = labels[~mask].view(labels.shape[0], -1)
-        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+        labels = labels[~mask].view(labels.shape[0], -1)  # (batch * n_views, batch * n_views - 1)
+        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)  # (batch * n_views, batch * n_views - 1)
         # assert similarity_matrix.shape == labels.shape
 
         # select and combine multiple positives
-        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)  # (batch * n_views, n_views - 1), here it's (batch * n_views, 1)
 
         # select only the negatives the negatives
-        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)  # (batch * n_views, (batch - 1) * n_views)
 
         logits = torch.cat([positives, negatives], dim=1)
-        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)  
+        # after rearrangement, only first entry is positive, so label is int(0); This used the assumption of `n_views=2` or the objective is wrong. 
 
         logits = logits / self.args.temperature
         return logits, labels
