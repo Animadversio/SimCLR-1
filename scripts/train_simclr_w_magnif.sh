187607a4bdd7652a5f2f2d91e938ea7064b0bc70
Animadversio <binxu.wang@wustl.edu>
2021-10-08 21:39:00 -0400

    add running script for replication with different random seed

--- scripts/train_simclr_w_magnif.sh
+++ scripts/train_simclr_w_magnif.sh
@@ -2,7 +2,7 @@
 #BSUB -n 4
 #BSUB -q general
 #BSUB -G compute-crponce
-#BSUB -J 'simclr_fast_magnif_exps[18-23]'
+#BSUB -J 'simclr_fast_magnif_exps[24-25]'
 #BSUB -gpu "num=1:gmodel=TeslaV100_SXM2_32GB:mode=exclusive_process"
 #BSUB -R 'gpuhost'
 #BSUB -R 'select[mem>48G]'
@@ -42,6 +42,8 @@ param_list=\
 --run_label proj256_eval_magnif_exp_cvr_0_01-0_15_slp_0_75-3_00 --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.15  --slope_C 0.75 3.0 --sampling_bdr 16
 --run_label proj256_eval_magnif_exp_cvr_0_05-1_50_slp_1_50      --magnif --gridfunc_form radial_exp --cover_ratio 0.05  1.50  --slope_C 1.5 --sampling_bdr 16
 --run_label proj256_eval_magnif_exp_cvr_0_05-1_50_slp_0_75-3_00 --magnif --gridfunc_form radial_exp --cover_ratio 0.05  1.50  --slope_C 0.75 3.0 --sampling_bdr 16
+--run_label proj256_eval_magnif_bsl_RND --randomize_seed --crop
+--run_label proj256_eval_magnif_cvr_0_05-0_35_RND --randomize_seed --magnif --cover_ratio 0.05 0.35  --fov_size 20  --K  20  --sampling_bdr 16
 '
 
 export extra_param="$(echo "$param_list" | head -n $LSB_JOBINDEX | tail -1)"
