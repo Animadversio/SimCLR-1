88a0d570ae8c5700d47322ce65e4be2e9ca531c7
Animadversio <binxu.wang@wustl.edu>
2021-10-09 04:22:56 -0400

    add tasks

--- scripts/train_simclr_w_magnif.sh
+++ scripts/train_simclr_w_magnif.sh
@@ -2,7 +2,7 @@
 #BSUB -n 4
 #BSUB -q general
 #BSUB -G compute-crponce
-#BSUB -J 'simclr_fast_magnif_exps[18-23,26-29]'
+#BSUB -J 'simclr_fast_magnif_exps[30-35]'
 #BSUB -gpu "num=1:gmodel=TeslaV100_SXM2_32GB:mode=exclusive_process"
 #BSUB -R 'gpuhost'
 #BSUB -R 'select[mem>48G]'
@@ -48,6 +48,12 @@ param_list=\
 --run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_3_00      --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 3.00  --sampling_bdr 16
 --run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_0_75-1_50 --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 0.75  1.5 --sampling_bdr 16
 --run_label proj256_eval_magnif_exp_cvr_0_01-0_35_slp_0_25-1_00 --magnif --gridfunc_form radial_exp --cover_ratio 0.01  0.35  --slope_C 0.25  1.0 --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T0_1_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 0.1 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T0_01_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 0.01 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T4_5_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 4.5 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T6_0_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 6.0 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T30_0_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 30.0 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
+--run_label proj256_eval_magnif_salmap_T100_0_cvr_0_01-0_35 --magnif --sal_sample --sample_temperature 100.0 --cover_ratio 0.01 0.35  --fov_size 20  --K  20  --sampling_bdr 16
 '
 
 export extra_param="$(echo "$param_list" | head -n $LSB_JOBINDEX | tail -1)"
